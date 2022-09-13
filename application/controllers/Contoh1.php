694346a05b79ad0e9f2c31f7525e506b2223574f
robi-rbz <robiaziz0110@bsi.ac.id>
2022-09-13 21:39:58 +0700

    Menambhakan file biodata di controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index(){
+        echo "Biodata <br>";
+        echo "Nama Saya Robi Aziz Zuama, Bapak bisa panggali saya Robi, saya dari program studi sistem informasi (S1)";
+    }
+}
