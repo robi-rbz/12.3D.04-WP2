fac63a63b1b1a8d41d0d3ca65db24984e574897c
robi-rbz <robiaziz0110@bsi.ac.id>
2022-09-20 21:18:03 +0700

    Pertemuan 2

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Biodata extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('v_form_bio');
 	}
+    public function cetak()
+    {
+        $data=[
+            'nim'=>$this->input->post('nim'),
+            'nama'=>$this->input->post('nama'),
+            'prodi'=>$this->input->post('prodi')
+        ];
+        $this->load->view('v_data_bio',$data);
+    }
 }
